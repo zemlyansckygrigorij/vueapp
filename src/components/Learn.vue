388f5bc1ba435f5d5ac916c0eae5f5b2e0fc7b6f
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2018-04-21 19:57:08 +0400

    Initial commit

---
+++ src/components/Learn.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="learn">
+
+  </div>
+
+</template>
+
+<script>
+  /* eslint-disable space-before-function-paren */
+
+  export default {
+    name: 'Learn',
+    props: {
+      msg: String
+    },
+    data () {
+      return {
+
+
+      }
+    },
+
+    methods: {
+
+    },
+    computed:{
+
+    },
+    watch: {
+
+    }
+  };
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
